71d4b0877d85c1ea5e892c10b6d890bceaa6f4b9
Rayelen-Eloi-Costa <costa.rayelen@gmail.com>
2022-07-04 13:15:13 -0300

    adicionado Modulo II e primeiras aulas

---
+++ Bootcamp Unimed BH/Modulo II/Aula Pratica/1 - PPDW/programa.php
@@ -0,0 +1,144 @@
+<html>
+	<header>
+		<title> Exercicio Final </title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+		<link rel="stylesheet" href="/resources/demos/style.css">
+		<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+		<script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+		<script>
+			$( function() {
+				$( "#accordion" ).accordion();
+			} );
+		</script>
+		<style type="text/css">
+			.titulo {
+				font-weight: bold;
+				color: #4C6189;
+				text-align: center;
+				font-family: Bradley Hand ITC;
+			}
+			.subtitulo {
+				font-weight: bold;
+				color: #727A8A;
+				text-decoration: underline;
+			}
+			.texto {
+				text-align: justify;
+			}
+			.destaque {
+				color: #754E8C;
+				text-decoration: underline;
+			}
+		</style>
+	</header>
+	
+	<body>
+		<! -- Titulo -->
+		<h1 class="titulo"> HORÓSCOPO </h1>
+		<h2> <center> Signos <center> </h2>
+		<h3 class="subtitulo"> O que são Signos? </h3>
+		<p class="texto">
+			O que se conhece como signo é na verdade o signo solar, que é a posição que o Sol ocupa em relação,
+			à Terra no momento do nascimento de cada pessoa. 
+		</p>
+		<p class="texto"> 
+			O signo solar sinaliza características da sua personalidade, mas você tem outros signos marcantes, 
+			como o ascendente ou a lua, por exemplo. 
+		</p>
+		<p class="texto">
+			Lendo a posição de todos os signos no seu Mapa Astral você pode mergulhar num caminho de 
+			autoconhecimento e entender quem de fato você é, como se relaciona e muito mais.
+		</p>
+		
+		<! -- Acoordion -->
+		<div id="accordion">
+		  <h3>Áries</h3>
+		  <div>
+			 <ul>
+				<li> <b class="destaque"> Características positivas:</b> Sinceridade, espontaneidade, pioneirismo e capacidade de canalizar atenções.</li>
+				<li> <b class="destaque"> Características negativas:</b> Agressividade, intolerância, impulsividade e egocentrismo.</li>
+			 </ul>
+		  </div>
+		  <h3>Touro</h3>
+		  <div>
+			 <ul>
+				<li> <b class="destaque"> Características positivas:</b> Solidez, firmeza, sensualidade e capacidade de realizar.</li>
+				<li> <b class="destaque"> Características negativas:</b> Teimosia, inércia, gula e materialismo.</li>
+			 </ul>
+		  </div>
+		  <h3>Gêmeos</h3>
+		  <div>
+			 <ul>
+				<li> <b class="destaque"> Características positivas:</b> Inteligência, perspicácia, versatilidade e poder de comunicação.</li>
+				<li> <b class="destaque"> Características negativas:</b> Especulações desnecessárias, inquietação, falta de concentração, e contradição.</li>
+			 </ul>
+		  </div>
+		  <h3>Câncer</h3>
+		  <div>
+			 <ul>
+				<li> <b class="destaque"> Características positivas:</b> Sensibilidade, intuição, zelo e boa memória.</li>
+				<li> <b class="destaque"> Características negativas:</b> Rancor, fobias, manipulação e fantasias exageradas.</li>
+			 </ul>
+		  </div>
+		  <h3>Leão</h3>
+		  <div>
+			 <ul>
+				<li> <b class="destaque"> Características positivas:</b> Generosidade, otimismo, nobreza e criatividade.</li>
+				<li> <b class="destaque"> Características negativas:</b> Melodramas, arrogância, egocentrismo e autoritarismo.</li>
+			 </ul>
+		  </div>
+		  <h3>Virgem</h3>
+		  <div>
+			 <ul>
+				<li> <b class="destaque"> Características positivas:</b> Perfeccionismo, sistematizações, inteligência analítica e postura estudiosa.</li>
+				<li> <b class="destaque"> Características negativas:</b> Obsessões compulsivas, criticismo, pedantismo e aprisionamento à lógica.</li>
+			 </ul>
+		  </div>
+		  <h3>Libra</h3>
+		  <div>
+			 <ul>
+				<li> <b class="destaque"> Características positivas:</b> Gentileza, cooperação e senso de proporção e de concórdia.</li>
+				<li> <b class="destaque"> Características negativas:</b> Indecisão, idealismo excessivo, inconstância e futilidade.</li>
+			 </ul>
+		  </div>
+		  <h3>Escorpião</h3>
+		  <div>
+			 <ul>
+				<li> <b class="destaque"> Características positivas:</b> Profundidade, intensidade, poder de transformação e vontade inabalável.</li>
+				<li> <b class="destaque"> Características negativas:</b> Morbidez, ressentimentos, sarcasmo e impulsos autodestrutivos.</li>
+			 </ul>
+		  </div>
+		  <h3>Sagitário</h3>
+		  <div>
+			 <ul>
+				<li> <b class="destaque"> Características positivas:</b> Percepção expandida, visão clara, intuição e fé.</li>
+				<li> <b class="destaque"> Características negativas:</b> Arrogância intelectual, tendência a excessos, fanatismo e intromissão.</li>
+			 </ul>
+		  </div>
+		  <h3>Capricórnio</h3>
+		  <div>
+			 <ul>
+				<li> <b class="destaque"> Características positivas:</b> Senso de responsabilidade, capacidade para o trabalho duro, sobriedade e praticidade.</li>
+				<li> <b class="destaque"> Características negativas:</b> Negativismo, mau humor, fatalismo e tendência a reclamar de tudo.</li>
+			 </ul>
+		  </div>
+		  <h3>Aquário</h3>
+		  <div>
+			 <ul>
+				<li> <b class="destaque"> Características positivas:</b> Originalidade, individualidade, independência e consciência social desenvolvida.</li>
+				<li> <b class="destaque"> Características negativas:</b> Utopias exageradas, radicalismo, tendência a chocar o outro e teorias impraticáveis.</li>
+			 </ul>
+		  </div>
+		  <h3>Peixes</h3>
+		  <div>
+			 <ul>
+				<li> <b class="destaque"> Características positivas:</b> Compaixão, simpatia, sensibilidade e tendências artísticas.</li>
+				<li> <b class="destaque"> Características negativas:</b> Negação da própria vida em prol de algo ou de alguém, autoilusão e dificuldade para encarar a realidade.</li>
+			 </ul>
+		  </div>
+		</div>
+	<br> <b> Informações retidas do site da <a href="https://www.personare.com.br/conteudo/o-que-sao-signos-m6247" target="_blank">personare.com.br</a> </b> </br>
+	</body>
+</html>
